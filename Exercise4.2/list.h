124b4a2f14c9a94441ba707cb3cdee7a668c1e20
vpetermio <vpetermio@yahoo.com>
2017-03-28 10:25:22 +0200

    added all linkedlist functions

--- Exercise4.2/list.h
+++ Exercise4.2/list.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <stdint.h>
 typedef struct node *list_p;
+
 void list_create(list_p *list);
-int add_item(void *); // Return 0 if item added else -1
-int remove_item(void *); // Return 0 if item removed else -1
-int no_of_items(); // Return number of items in list
-void *get_item(void *item);
+void list_destroy(list_p *head);
+int add_item(list_p *head,void *item); // Return 0 if item added else -1
+int remove_item(list_p *head,void *item); // Return 0 if item removed else -1
+int no_of_items(list_p *head); // Return number of items in list
+list_p get_iterator(list_p *list);
+int has_next(list_p *iterator);
+void *next(list_p *iterator);
+void *get_item(list_p *head, uint16_t index);
 void toString();
